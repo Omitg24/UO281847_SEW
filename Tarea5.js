7d7d7215e41ddaa49d6dc03123cfeac35d5e9cf9
Omitg24 <omartg24@gmail.com>
2022-12-06 01:11:50 +0100

    05/12/2022 Pruebas de JS sección 2

---
+++ Tarea5.js
@@ -0,0 +1,28 @@
+class Mapa {
+    initMap() {
+        mapboxgl.accessToken = 'pk.eyJ1Ijoib21pdGciLCJhIjoiY2xiNWF4OWp4MDE2bDNub2FlbHp3dmZvcyJ9.mQp5NnAxt9CuOm7GuD1ODg';
+        $("header").after("<main id=\"mapa\"></main>");
+        var map = new mapboxgl.Map({
+            container: "mapa",
+            style: "mapbox://styles/mapbox/streets-v9",
+            center: [-5.693909,43.311065], 
+            zoom: 8 
+        });
+
+        map.addControl(
+            new mapboxgl.GeolocateControl({
+                positionOptions: {
+                    enableHighAccuracy: true
+                },                
+                trackUserLocation: true,                
+                showUserHeading: true
+            })
+        );    
+            
+        const marker1 = new mapboxgl.Marker()
+        .setLngLat([-5.693909,43.311065])
+        .addTo(map);
+    }
+}
+
+new Mapa().initMap();
